fddaedfe504cc1ae2a230f0702c29bf4ea6a0821
samuel <samucabyte@gmail.com>
2025-09-13 12:14:18 -0300

    Portfólio Next.js inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import About from "@/components/About"
 import Contact from "@/components/Contact"
 import Hero from "@/components/Hero"
 import Navbar from "@/components/NavBar"
-import Projects from "@/components/project"
+import Projects from "@/components/Project"
 
 function App() {
   return (
